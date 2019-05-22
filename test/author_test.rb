a58684b36383ad28841609156d01acb8555e9122
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 15:11:19 -0600

    Add tests for writing a book

--- test/author_test.rb
+++ test/author_test.rb
@@ -19,4 +19,12 @@ class AuthorTest < Minitest::Test
     assert_equal [], @charlotte_bronte.books
   end
 
+  def test_write_book
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = @charlotte_bronte.write("Villette", "1853")
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre.title
+    assert_equal [jane_eyre, villette], @charlotte_bronte.books
+  end
+
 end
