8a875660cd79eed67760e3cfcafb74449f7e6dc9
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 14:10:33 -0600

    Initialize the class and add attr_readers

---
+++ lib/book.rb
@@ -0,0 +1,10 @@
+class Book
+attr_reader :title, :author, :publication_year
+
+  def initialize(info)
+    @title = info[title]
+    @author = info[author]
+    @publication_year = info[publication_year]
+  end
+
+end
