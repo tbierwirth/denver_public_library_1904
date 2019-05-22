9fa98942b6650439670c5adc5553803fe5d87061
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 15:40:48 -0600

    Add method to add authors and their books to the library

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,18 @@
+require 'pry'
 class Library
-  attr_reader :name, :books
+  attr_reader :name, :books, :authors
 
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
+    author.books.each do |book|
+      @books << book
+    end
   end
 
 end
