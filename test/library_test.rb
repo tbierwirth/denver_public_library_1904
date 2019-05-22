a45de503ca1da354819f890d092601541f6dcb52
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 15:13:54 -0600

    Initialize test and add test for instance

---
+++ test/library_test.rb
@@ -0,0 +1,18 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
+require './lib/author'
+require './lib/library'
+
+class LibraryTest < Minitest::Test
+
+  def setup
+    @dpl = Library.new("Denver Public Library")
+  end
+
+  def test_it_exists
+    assert_instance_of Library, @dpl
+  end
+
+end
