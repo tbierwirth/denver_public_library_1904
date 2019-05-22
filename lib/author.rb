f63c93ab873e5de9b2de6b82a26222176d402a51
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 14:26:29 -0600

    Add attributes and method for name

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,15 @@
 class Author
-  attr_reader :first_name, :last_name
+
+  attr_reader :first_name, :last_name, :books
+
   def initialize(info)
-    @first_name = first_name
-    @last_name = last_name
+    @first_name = info[:first_name]
+    @last_name = info[:last_name]
+    @books = []
+  end
+
+  def name
+    "#{first_name} #{last_name}"
   end
 
 end
